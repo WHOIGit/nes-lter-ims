311d4cd66ef99fba94a3a1f4c5b9e61bb6c403e1
Joe Futrelle <joefutrelle@gmail.com>
2018-08-31 11:30:26 -0400

    supporting neslter chl spreadsheet

--- neslter/parsing/chl.py
+++ neslter/parsing/chl.py
@@ -1,40 +1,60 @@
 import pandas as pd
+import numpy as np
 
-from .utils import clean_column_names, drop_columns, dropna_except, cast_columns, format_dataframe
+from .utils import dropna_except, clean_column_names, cast_columns, float_to_datetime, format_dataframe
 
 """Parsing chlorophyll Excel spreadsheet"""
 
-# FIXME this is based on SFDchl, should be based on NESLTERchl?"""
-
-# columns expected
-CHL_COLUMNS = ['Cruise #:', 'Cast #', 'Niskin #', 'Target\nDepth', 'Replicate',
-       'Filter\nSize', 'Vol\nFilt', 'Vol Extracted', 'Sample', '90% Acetone',
-       'Dilution During Reading', 'Chl_Cal_Filename', 'tau_Calibration',
-       'Fd_Calibration', 'Rb', 'Ra', 'blank', 'Rb-blank', 'Ra-blank',
-       'Chl (ug/l)', 'Phaeo (ug/l)', 'Cal_Date', 'Fluorometer',
-       'Lab notebook and page number', 'Comments', 'Sample.1',
-       '90% Acetone.1']
+RAW_COLS = ['Cruise #:', 'Date', 'LTER\nStation', 'Cast #', 'Niskin #', 'Time\nIn',
+       'Time\nOut', 'Replicate', 'Vol\nFilt', 'Filter\nSize', 'Vol Extracted',
+       'Sample', '90% Acetone', 'Dilution During Reading', 'Chl_Cal_Filename',
+       'tau_Calibration', 'Fd_Calibration', 'Rb', 'Ra', 'blank', 'Rb-blank',
+       'Ra-blank', 'Chl (ug/l)', 'Phaeo (ug/l)', 'Cal_Date',
+       'Personnel\nFilter', 'Personnel\nRead', 'Fluorometer', 'Comments',
+       'Unnamed: 29']
 
 def parse_chl(chl_xl_path):
-    df = pd.read_excel(chl_xl_path)
-    assert set(df.columns) == set(CHL_COLUMNS), 'chl spreadsheet does not contain expected columns'
-    # clean column names
-    df = clean_column_names(df, {
-        'Vol\nFilt': 'vol_filtered', # unabbreviating to be consistent with vol_extracted
-        '90% Acetone': 'ninety_percent_acetone', # spell initial number
-        'Chl (ug/l)': 'chl', # remove units
-        'Phaeo (ug/l)': 'phaeo', # remove units
-        })
-    # from now on, clean column names will be used
-    # drop unused columns
-    df = drop_columns(df, ['sample_1', '_90_acetone_1', 'fluorometer'])
-    # drop nas
-    df = dropna_except(df, ['comments'])
-    # convert selected columns to ints
-    int_cols = ['cast','niskin','target_depth','filter_size',
-        'vol_filtered','vol_extracted','sample','ninety_percent_acetone']
-    cast_columns(df, int, int_cols)
+    raw = pd.read_excel(chl_xl_path)
+    assert set(raw.columns) == set(RAW_COLS), 'chl spreadsheet does not contain expected columns'
+    df = clean_column_names(raw, {
+        'Vol\nFilt': 'vol_filtered',
+        'Chl (ug/l)': 'chl',
+        'Phaeo (ug/l)': 'phaeo',
+        'Unnamed: 29': 'comments_2',
+        '90% Acetone': 'ninety_percent_acetone'
+    })
+    # drop rows with nas
+    na_allowed = ['lter_station', 'comments', 'comments_2', 'personnel_filter', 'personnel_read']
+    df = dropna_except(df, na_allowed)
+    # cast the int columns
+    df = cast_columns(df, int, ['filter_size'])
+    df['date'] = float_to_datetime(df['date'])
+    df['cal_date'] = float_to_datetime(df['cal_date'])
+    str_cols = na_allowed + ['cast', 'sample']
+    df = cast_columns(df, str, str_cols, fillna='')
+    # deal with 'freeze' in time_in and time_out columns
+    # add freeze column
+    freeze = df['time_in'].str.lower() == 'freeze'
+    df['freeze'] = freeze
+    for c in ['time_in', 'time_out']:
+        df.loc[freeze, c] = np.nan
+        df[c] = pd.to_datetime(df[c])
     return df
+
+def validate_chl(df):
+    q = df['fd_calibration']
+    p = df['tau_calibration']
+    u = df['rb_blank']
+    v = df['ra_blank']
+    k = df['vol_extracted']
+    i = df['vol_filtered']
+    n = df['dilution_during_reading']
+
+    expected_chl = ( q * p / (p - 1) * (u - v) * k / i) / n
+    assert np.allclose(df['chl'], expected_chl), 'unexpected chl value(s)'
+
+    expected_phaeo = ( q * p / ( p - 1) * (p * v - u) * k / i) / n
+    assert np.allclose(df['phaeo'], expected_phaeo), 'unexpected phaeo value(s)'
 
 def format_chl(df):
     return format_dataframe(df, precision={
