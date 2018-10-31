36bc33c66313269bf0260bb97b570c22ed5c219a
Joe Futrelle <joefutrelle@gmail.com>
2018-10-31 16:52:11 -0400

    fixed bug where it wasn't cleaning column names

--- neslter/parsing/ctd/asc.py
+++ neslter/parsing/ctd/asc.py
@@ -4,7 +4,7 @@ from ..utils import clean_column_names
 
 def parse_asc(asc_path, delimiter=';'):
     df = pd.read_csv(asc_path, encoding='latin-1', delimiter=delimiter)
-    clean_column_names(df)
+    df = clean_column_names(df)
     return df
 
 def format_asc(df):
