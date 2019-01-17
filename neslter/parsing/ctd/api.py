e097574fe0b172cb48b24c53275af0da06b546e7
Joe Futrelle <joefutrelle@gmail.com>
2019-01-17 13:09:04 -0500

    added some comments

--- neslter/parsing/ctd/api.py
+++ neslter/parsing/ctd/api.py
@@ -11,10 +11,14 @@ class Ctd(object):
         # return a list of the cast numbers
         return sorted(self.metadata()['cast'].unique())
     def cast(self, cast_number):
+        # return data for a specific cast
         return parse_cast(self.dir, cast_number)
     def bottles(self, **kw):
+        # return data for each bottle
         return compile_btl_files(self.dir, **kw)
     def bottle_summary(self, **kw):
+        # summarize bottle data
         return summarize_compiled_btl_files(self.bottles())
     def metadata(self):
+        # return basic metadata from the header files
         return compile_hdr_files(self.dir)
