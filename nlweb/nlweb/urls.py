938c7dea1f8b63b527a063ad1a14f0dd31008348
Joe Futrelle <joefutrelle@gmail.com>
2019-02-27 09:05:22 -0500

    root redirects to github wiki

--- nlweb/nlweb/urls.py
+++ nlweb/nlweb/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic.base import RedirectView
+
+HOME_URL = 'https://github.com/WHOIGit/nes-lter-ims/wiki'
+
 urlpatterns = [
+    path('', RedirectView.as_view(url=HOME_URL), name='home'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
