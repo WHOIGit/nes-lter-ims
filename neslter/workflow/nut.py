2378bb1a2688e07a796cc825e24c2ca2552c6d43
Joe Futrelle <joefutrelle@gmail.com>
2021-05-07 13:53:25 -0400

    sample log now an xlsx file

--- neslter/workflow/nut.py
+++ neslter/workflow/nut.py
@@ -22,6 +22,6 @@ class NutPlusBottlesWorkflow(Workflow):
         bottle_summary = CtdBottleSummaryWorkflow(self.cruise).get_product()
         nut_path = Resolver().raw_file(NUT, 'LTERnut.xlsx')
         parent_dir = os.path.dirname(os.path.dirname(nut_path)) # ..
-        sample_log_path = os.path.join(parent_dir, 'LTER_sample_log.xls')
+        sample_log_path = os.path.join(parent_dir, 'LTER_sample_log.xlsx')
         merged = merge_nut_bottles(sample_log_path, nut_path, bottle_summary, self.cruise)
         return add_nearest_station(self.cruise, merged)
