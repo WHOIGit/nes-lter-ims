1842f2d6db3751186264cb65cba046b292f25f23
joefutrelle <joefutrelle@gmail.com>
2023-04-03 17:00:26 -0400

    configuring default sqlite

--- nlweb/nlweb/settings.py
+++ nlweb/nlweb/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'nlweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase', # This is where you put the name of the db file. 
-                 # If one doesn't exist, it will be created at migration time.
+        'NAME': 'neslter',
     }
 }
 
