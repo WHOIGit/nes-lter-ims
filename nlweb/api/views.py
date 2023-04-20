aa99cd8671667810a2020642fa233af582601f04
Joanne Koch <jkoch@whoi.edu>
2023-04-20 11:57:52 -0400

    remove debug prints

--- nlweb/api/views.py
+++ nlweb/api/views.py
@@ -64,16 +64,7 @@ def workflow_response(workflow, extension=None):
         df = workflow.get_product()
         return dataframe_response(df, filename, extension)
     except DataNotFound as e:
-        sys.tracebacklimit = 0
-        print(traceback.format_exc())    # print DataNotFound message to console
-        print("404 error in workflow_response")  #missing elog or ctd dir or invalid cruise
         raise Http404(str(e))
-    except ValueError as e: 
-        print(e)              # prints invalid elog file contents error for AR61a        
-        raise Http404(str(e))  
-    except Exception as e:
-        print (e)             # prints elog file not found for AR16
-        raise Http404(str(e))  
 
 def cruises(request):
     cruises = Resolver().cruises()
